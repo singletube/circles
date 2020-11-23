0cb922f240dc4dc9c908665f58d23b6f399331e7
singletube <74777397+singletube@users.noreply.github.com>
2020-11-23 16:59:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,78 @@
-def main():
-    from PyQt5 import QtCore, QtGui, QtWidgets
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '11.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
 
 
-    class Ui_MainWindow(object):
-        def setupUi(self, MainWindow):
-            MainWindow.setObjectName("MainWindow")
-            MainWindow.resize(800, 600)
-            self.centralwidget = QtWidgets.QWidget(MainWindow)
-            self.centralwidget.setObjectName("centralwidget")
-            self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-            self.pushButton.setGeometry(QtCore.QRect(300, 210, 171, 81))
-            self.pushButton.setObjectName("pushButton")
-            MainWindow.setCentralWidget(self.centralwidget)
-            self.menubar = QtWidgets.QMenuBar(MainWindow)
-            self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-            self.menubar.setObjectName("menubar")
-            MainWindow.setMenuBar(self.menubar)
-            self.statusbar = QtWidgets.QStatusBar(MainWindow)
-            self.statusbar.setObjectName("statusbar")
-            MainWindow.setStatusBar(self.statusbar)
-
-            self.retranslateUi(MainWindow)
-            QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-        def retranslateUi(self, MainWindow):
-            _translate = QtCore.QCoreApplication.translate
-            MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-            self.pushButton.setText(_translate("MainWindow", "Кнопка"))
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-    import sys
-    from random import randint
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(300, 210, 171, 81))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-    from PyQt5.QtGui import QPainter, QColor
-    from PyQt5.QtWidgets import QApplication, QMainWindow
-    from main import Ui_MainWindow
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Кнопка"))
 
-    class MyWidget(QMainWindow, Ui_MainWindow):
-        def __init__(self):
-            super().__init__()
-            self.setupUi(self)
-            self.pushButton.clicked.connect(self.paint)
-            self.do_paint = False
+import sys
+from random import randint
 
-        def paintEvent(self, event):
-            if self.do_paint:
-                qp = QPainter()
-                qp.begin(self)
-                self.draw_flag(qp)
-                qp.end()
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from main import Ui_MainWindow
 
-        def paint(self):
-            self.do_paint = True
-            self.repaint()
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
 
-        def draw_flag(self, qp):
-            for i in range(randint(1, 10)):
-                p = randint(0, 300)
-                h = randint(0, 300)
-                qp.setBrush(QColor(255, 255, 0))
-                qp.drawEllipse(p, p, h, h)
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
 
+    def draw_flag(self, qp):
+        for i in range(randint(1, 10)):
+            p = randint(0, 300)
+            h = randint(0, 300)
+            qp.setBrush(QColor(255, 255, 0))
+            qp.drawEllipse(p, p, h, h)
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
-
-    def except_hook(cls, exception, traceback):
-        sys.__excepthook__(cls, exception, traceback)
-
-
-    if __name__ == '__main__':
-        app = QApplication(sys.argv)
-        form = MyWidget()
-        form.show()
-        sys.excepthook = except_hook
-        sys.exit(app.exec())
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    form = MyWidget()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
